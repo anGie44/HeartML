37890577e345c56c686a170f6eba556e68fa7c61
anGie44 <angelinepinilla@gmail.com>
2016-02-14 18:17:09 -0500

    Update fourierBased.py

--- fourierBased.py
+++ fourierBased.py
@@ -8,3 +8,10 @@ import sys
 import random
 from matplotlib import image
 from scipy.ndimage import label
+from scipy.ndimage.morphology import binary_erosion
+from scipy.fftpack import fftn, ifftn
+from scipy.signal import argrelmin, correlate
+from scipy.spatial.distance import euclidean
+from scipy.stas import linregress
+from scipy.optimize import curve_fit
+from scipy.interpolate import UnivariateSpline 
