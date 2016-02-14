5c03caaebbfbab48f401a20254a58cd0f9758280
anGie44 <angelinepinilla@gmail.com>
2016-02-14 18:27:43 -0500

    Create DataManip.py

---
+++ DataManip.py
@@ -0,0 +1,8 @@
+class Dataset(object):
+  dataset_count = 0
+  
+  def __init__(self, ):
+  def _filename(self, s, t):
+  def _read_dicom_image(self, filename):
+  def _read_all_dicom_imageS(self):
+  def load(self):
