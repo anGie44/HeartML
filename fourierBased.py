b3270e0014ab4f655d3a68480b40e8b32f993e00
anGie44 <angelinepinilla@gmail.com>
2016-02-14 19:33:33 -0500

    Update fourierBased.py

--- fourierBased.py
+++ fourierBased.py
@@ -12,7 +12,7 @@ from scipy.ndimage.morphology import binary_erosion
 from scipy.fftpack import fftn, ifftn
 from scipy.signal import argrelmin, correlate
 from scipy.spatial.distance import euclidean
-from scipy.stas import linregress
+from scipy.stats import linregress
 from scipy.optimize import curve_fit
 from scipy.interpolate import UnivariateSpline 
 from dataManip import *
